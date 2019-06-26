65e61de8eb291516fecfb2e1fdb73605920667f2
fernando pimentel <ferpiolli@gmail.com>
2019-06-26 08:20:31 -0300

    javaScript

--- aula1/app.js
+++ aula1/app.js
@@ -1,24 +1,53 @@
 
-alert("Eu sou um alerta java script");
-var nome = document.getElementById("nome").value;
-var email = document.getElementById("email").value;
+//alert("Eu sou um alerta java script");
+// //var nome = document.getElementById("nome").value;
 
-function bemVindo() {
-    alert("Bem Vindo ao mundo JavaScript")
-}
 
-function mostraNome() {
+// function bemVindo() {
+//     alert("Bem Vindo ao mundo JavaScript")
+// }
+
+// function mostraNome() {
+//     var nome = document.getElementById("nome").value;
+//     alert(nome);
+// }
+
+function enviaCadastro(event) {
     var nome = document.getElementById("nome").value;
-    alert(nome);
-}
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    var sexo = "";
+    var sM = document.getElementById("masculino").checked;
+    var sF = document.getElementById("feminino").checked;
 
-function enviaCadastro() {
-    var nome = document.getElementById("nome").value;
-    if ( nome.length <= 0 ) {
-        alert('nome deve ser informado');
-    }else if(email.lenght <= 0 ){
-        alert('email de ser informado');
-    } else{
-        alert('cadastro enviado com sucesso');
+    if (sM == true){
+        sexo = 'Masculino'
     }
+    if (sF == true){
+        sexo = 'Feminino'
+    }
+    var novoCadastro = {
+        nome : nome,
+        email : email,
+        senha: senha,
+        celular: document.getElementById("celular").value,
+        tipo_contato: document.getElementById("tipoContato").value,
+        sexo: sexo,
+        info: document.getElementById("info").value,
+        news: document.getElementById("news").checked
+
+    }
+    if (nome.length <= 0) {
+        alert('nome deve ser informado');
+    } else if (email.length <= 0) {
+        alert('email deve ser informado');
+    } else if (senha.length <= 0) {
+        alert('senha deve ser informada');
+    }
+
+    else {
+        alert(JSON.stringify(novoCadastro));
+    }
+
+  
 }
