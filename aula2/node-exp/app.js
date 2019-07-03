59fef885fd962f3546cbf7ca027a6207bdf08d37
fernando pimentel <ferpiolli@gmail.com>
2019-07-03 08:42:04 -0300

    aula2 nodejs criando api com express

--- aula2/node-exp/app.js
+++ aula2/node-exp/app.js
@@ -5,19 +5,29 @@ const app = express();
 app.get('/', function(req, res){
     return res.json({msg:'Ola node com Express'});
 
-});
+})
 
 app.get('/users', function(req, res){
     var users=[
-        {id: 1, name: "Fernando", email: "fernando@teste.com"},
-        {id: 2, name: "camila", email: "camila@teste.com"},
+        {id: 1, name: "Fernando", email: "fernando@test.com"},
+        {id: 2, name: "camila", email: "camila@test.com"},
         {id: 3, name: "bruce", email: "bruce@test.com"}
     ]
-    return res.json(users)
-});
+    return res.json(users);
+})
 
-app.get('/users:id', function(req,res){
-    return res.json(req.params.id)
+app.get('/users/:id', function(req,res){
+    var user = {id:1, name: "Fernando", email: "fernando@test.com"}
+    if (user.id == req.params.id){
+        res.json(user)
+    }else {
+        res.status(404).json({msg: 'usuário não encontrado. '})
 
+    }
+
+})
+app.post('/users', function(req, res){
+    console.log(req.body);
+    return res.json(req.body)
 })
 app.listen(3000);
