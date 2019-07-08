937b0850383cc71b278a37dcd942c56472d7412a
fernando pimentel <ferpiolli@gmail.com>
2019-07-08 07:28:36 -0300

    aula2 /get /post

--- aula2/node-exp/app.js
+++ aula2/node-exp/app.js
@@ -1,33 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', function(req, res){
-    return res.json({msg:'Ola node com Express'});
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    return res.json({ msg: 'Ola node com Express' });
 
 })
 
-app.get('/users', function(req, res){
-    var users=[
-        {id: 1, name: "Fernando", email: "fernando@test.com"},
-        {id: 2, name: "camila", email: "camila@test.com"},
-        {id: 3, name: "bruce", email: "bruce@test.com"}
+app.get('/users', function (req, res) {
+    var users = [
+        { id: 1, name: "Fernando", email: "fernando@test.com" },
+        { id: 2, name: "camila", email: "camila@test.com" },
+        { id: 3, name: "bruce", email: "bruce@test.com" }
     ]
     return res.json(users);
 })
 
-app.get('/users/:id', function(req,res){
-    var user = {id:1, name: "Fernando", email: "fernando@test.com"}
-    if (user.id == req.params.id){
+app.get('/users/:id', function (req, res) {
+    var user = { id: 1, name: "Fernando", email: "fernando@test.com" }
+    if (user.id == req.params.id) {
         res.json(user)
-    }else {
-        res.status(404).json({msg: 'usuário não encontrado. '})
+    } else {
+        res.status(404).json({ msg: 'usuário não encontrado. ' })
 
     }
 
 })
 app.post('/users', function(req, res){
-    console.log(req.body);
-    return res.json(req.body)
+    var newUser = req.body;
+    newUser.id= '4'
+    return res.json(newUser)
+
 })
 app.listen(3000);
