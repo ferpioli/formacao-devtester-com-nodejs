ce8c84279cdf6bd7ab0519df7f44277bbf3ffd70
fernando pimentel <ferpiolli@gmail.com>
2019-07-11 08:45:28 -0300

    correção de erro na string de conexão com mongo

--- aula3/cineapi/app.js
+++ aula3/cineapi/app.js
@@ -5,7 +5,7 @@ import bodyparser from'body-parser';
 const app = express();
 const mongoose = require('mongoose');
 app.use(bodyparser.urlencoded({extended: false}));
-mongoose.connect('mongdb://localhost/cinedb', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/cinedb', { useNewUrlParser: true });
 
 app.listen(3000, ()=>{
     console.log('cineApi esta no ar');
