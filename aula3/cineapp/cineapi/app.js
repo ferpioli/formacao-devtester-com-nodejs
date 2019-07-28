6c62069b98cb4a7add141dad44de17eeed1df574
fernando pimentel <ferpiolli@gmail.com>
2019-07-28 10:28:32 -0300

    integrando a api com o axios

--- aula3/cineapp/cineapi/app.js
+++ aula3/cineapp/cineapi/app.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import crud from "./crud";
 
 
 import Movie from './models/movie';
 const app = express();
+
+app.use(cors({
+    origins: ["*"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}))
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
