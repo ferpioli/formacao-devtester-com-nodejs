88e6c322e3d3da8a1d1fbd47a236f916338f2bac
fernando pimentel <ferpiolli@gmail.com>
2019-07-27 23:44:18 -0300

    criando aplicação front-end com vuejs

---
+++ aula3/cineapp/cineweb/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    
+    <MovieList/>
+  </div>
+</template>
+
+<script>
+
+import MovieList from './components/MovieList'
+
+export default {
+  name: 'App',
+  components: {
+ 
+    MovieList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ aula3/cineapp/cineweb/src/components/MovieList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="movies">
+    <h1>{{ title }}</h1>
+    <p>{{welcome}}</p>
+
+    <table class="tabela"> 
+      <thead>
+        <th>Nome</th>
+        <th>Ano</th>
+        <th>Sinopse</th>
+      </thead>
+      <tbody>
+        <tr v-for="movie in movies.data" :key="movie.id"> 
+          <td>{{movie.name}}</td>
+          <td>{{movie.year}}</td>
+          <td>{{movie.plot}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieList",
+  data() {
+    return {
+      title: "cadastro de filmes",
+      welcome: "seja bem vindo!",
+      movies: {
+        data: [
+          { id: 1,
+            name: "Deadpool2",
+            year: 2018,
+            cast: ["Ryan Reynolds", "Josh Brolin"],
+            plot: "O Super soldado Cable vem do futuro com a missão..."
+          },
+          { id:2,
+            name: "Vingadores Era de Ultron",
+            year: 2015,
+            cast: ["Robert Downey Jr.", "Eliabeth Olsen"],
+            plot: "Ao tentar proteger o planeta de ameaças Tony Stark..."
+          },
+          { id:3,
+            name: "Vingadores Endgame",
+            year: 2019,
+            cast: ["Robert Downey Jr.", "Chris Evans"],
+            plot:
+              "Apos Thanos eliminar metade das criaturas vivas os vingadores..."
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.tabela{
+    display:inline-block
+}
+
+</style>
